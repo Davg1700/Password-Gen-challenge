977874e61788983de78b4c3ee30ace17998b00bf
Davg1700 <davg1700@gmail.com>
2022-10-21 12:26:05 -0700

    password promts

--- Devleope/script.js
+++ Devleope/script.js
@@ -1,38 +1,91 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// function 
-  function generatePassword(){
-    //char vars
+function generatePassword(){
+    //cahraters for each value type
     var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
     var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     var numbers = '0123456789';
     var specialChar = '!@#$%^&*_-+=';
-  
-    //setting password length
-    var passwordLenght = prompt('set password let from 8 to 128 please');
-    
-    //ensuring pass feilds are correct
-    if(!passwordLenght){
-      //restars if user hits cancel
-        alert("Hit the start button to start again") 
+
+    // Allows user to input password length 
+    var passLength = prompt("Set your password length(8 - 128).");
+
+    //ensures for vaild entry
+    if(!passLength){
+      //alows user to try again
+        alert("try again.") 
         return;
-      //checking if its a valid entry
+      // ensure user sets corect values
     } else if(passLength < 8 || passLength > 129) {
-        alert("You have entered an invaled entry.")
-        return; 
+      alert("Entered an invaild entry try again.")
+      return; 
     }
   
-    for (let i = 0; i < passwordLenght; i++){
-      let randomNumber = Math.floor(Math.random()
-      *chars.length);
-  
-      password += chars.substring(randomNumber, randomNumber + 10);
-      console.log(password);
-      //displaypass
-      return password;
+
+    // cahrater select prompts
+    var lowerCaseSelect = confirm("Would you like lower case chars select confirm for yes cancle for no.")
+    var upperCaseSelect = confirm("Would you like upper case chars select confirm for yes cancle for no.")
+    var numberSelect = confirm("Would you like number values select confirm for yes cancle for no.")
+    var specialSelect = confirm("Would you like special chars select confirm for yes cancle for no.")
+    
+    //ensures 1 charater is selected
+    if(!lowerCaseSelect && !upperCaseSelect && !numberSelect && !specialSelect){
+      alert("Selecet at leset 1 charater.")
+      return;
     }
-  }
+
+    // user selections for password
+
+    if(lowerCaseSelect){
+      var lowerCaseSelectConfirm = "Yes";
+    } else {
+      var lowerCaseSelectConfirm = "No";
+      lowerCase = '';
+    }
+
+    if(upperCaseSelect){
+      var upperSelectConfirm = "Yes";
+    } else {
+      var upperSelectConfirm = "No";
+      upperCase = '';
+    }
+
+    if(numberSelect){
+      var numberSelectConfirm = "Yes";
+    } else {
+      var numberSelectConfirm = "No";
+      numbers = '';
+    }
+
+    if(specialSelect){
+      var specialSelectConfirm = "Yes";
+    } else {
+      var specialSelectConfirm = "No";
+      specialChar = '';
+    }
+
+    // comfim promt for user to ensure their password has selected feilds they chose
+    var passConfirm = confirm("These are your selected parameters: \n Password Length: " + passLength + "\n Lower Case: " + lowerCaseSelectConfirm + "\n Upper Case: " + upperSelectConfirm + "\n Numbers: " + numberSelectConfirm + "\n Special Characters: " + specialSelectConfirm + "\nIf you'd like to proceed, press ok.");
+
+    //lets user start again to with selection from begining
+    if(!passConfirm){
+      alert("Please start again.");
+      return;
+    }
+
+    //new password with selected feilds
+    var newPass = lowerCase + upperCase + numbers + specialChar;
+    //password
+    var password = ''
+
+    //random pass word gen with entered entry
+    for ( var i = 0; i < passLength; i++ ) {
+      password += newPass.charAt(Math.floor(Math.random() * newPass.length)); 
+    }
+  return password;
+}
+
 
 // Write password to the #password input
 function writePassword() {
@@ -45,3 +98,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
